26d6036c9e07d3da6c86b136f889523a4762e675
Johannes Stoljar <johannes.stoljar@uni-a.de>
2023-03-10 08:21:04 +0100

    Bug fix

--- test/FMI2/eval.jl
+++ test/FMI2/eval.jl
@@ -3,7 +3,7 @@ using FMI
 
 config = Configuration(; julia="1.8");
 
-package = Package(; name="FMI.jl");
+package = Package(; name="FMI");
 
 @info "PkgEval"
 println(evaluate([config], [package]))
