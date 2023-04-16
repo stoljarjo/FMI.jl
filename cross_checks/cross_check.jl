38cf46db7eeae6bbfa0c970db3dc01506d732024
stoljarjo <stoljarjo@users.noreply.github.com>
2023-04-16 20:21:51 +0200

    Bug fix

--- cross_checks/cross_check.jl
+++ cross_checks/cross_check.jl
@@ -217,10 +217,14 @@ function main()
     cross_check_repo_name = get(ENV, "CROSS_CHECK_REPO_NAME", "")
     cross_check_repo_user = get(ENV, "CROSS_CHECK_REPO_USER", "")
     if github_token != "" && cross_check_repo_name != "" && cross_check_repo_user != ""
-        println("SET shh comando")
-        run(Cmd(`$(git()) config core.sshCommand 'ssh -i $pkey_filename' `, dir=fmiCrossCheckRepoPath))
-        println("SET remote url")
-        run(Cmd(`$(git()) remote set-url origin git@github.com:$cross_check_repo_user/$cross_check_repo_name`, dir=fmiCrossCheckRepoPath))
+        withenv(
+            "GIT_SSH_COMMAND" => isnothing(github_token) ? "ssh" : "ssh -i $pkey_filename -o StrictHostKeyChecking=no"
+        ) do
+            run(
+                Cmd(`$(git()) remote set-url origin git@github.com:$cross_check_repo_user/$cross_check_repo_name`, dir=fmiCrossCheckRepoPath)
+            )
+        end
+
         try
             run(Cmd(`$(git()) checkout $(crossCheckBranch)`, dir=fmiCrossCheckRepoPath))
         catch
@@ -272,15 +276,16 @@ function main()
     if github_token != "" && cross_check_repo_name != "" && cross_check_repo_user != ""
         run(Cmd(`$(git()) add -A`, dir=fmiCrossCheckRepoPath))
         run(Cmd(`$(git()) commit -a --allow-empty -m "Run FMI cross checks for FMI.JL"`, dir=fmiCrossCheckRepoPath))
-        # println()
-        # run(Cmd(`$(git()) config core.sshCommand 'ssh -i $pkey_filename' `, dir=fmiCrossCheckRepoPath))
         
-        try
-            run(Cmd(`$(git()) push`, dir=fmiCrossCheckRepoPath))
-        catch
-            run(Cmd(`$(git()) push --set-upstream origin $(crossCheckBranch)`, dir=fmiCrossCheckRepoPath))
+        withenv(
+            "GIT_SSH_COMMAND" => isnothing(github_token) ? "ssh" : "ssh -i $pkey_filename -o StrictHostKeyChecking=no"
+        ) do
+            try
+                run(Cmd(`$(git()) push`, dir=fmiCrossCheckRepoPath))
+            catch
+                run(Cmd(`$(git()) push --set-upstream origin $(crossCheckBranch)`, dir=fmiCrossCheckRepoPath))
+            end
         end
-
         rm(tmp_dir; force=true, recursive=true)
     end
 end
