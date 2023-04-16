9b9c34a1a7b5531d65e9f893acd11753a48d9da0
stoljarjo <stoljarjo@users.noreply.github.com>
2023-04-16 20:28:57 +0200

    Add UserKnownHostsFile=/dev/null

--- cross_checks/cross_check.jl
+++ cross_checks/cross_check.jl
@@ -218,8 +218,9 @@ function main()
     cross_check_repo_user = get(ENV, "CROSS_CHECK_REPO_USER", "")
     if github_token != "" && cross_check_repo_name != "" && cross_check_repo_user != ""
         withenv(
-            "GIT_SSH_COMMAND" => isnothing(github_token) ? "ssh" : "ssh -i $pkey_filename -o StrictHostKeyChecking=no"
+            "GIT_SSH_COMMAND" => isnothing(github_token) ? "ssh" : "ssh -i $pkey_filename -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null"
         ) do
+            # run(Cmd(`ssh-keyscan -t rsa github.com >> ~/.ssh/known_hosts`))
             run(
                 Cmd(`$(git()) remote set-url origin git@github.com:$cross_check_repo_user/$cross_check_repo_name`, dir=fmiCrossCheckRepoPath)
             )
